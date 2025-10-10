836a9b7081348b6f74cd24ccf40a167592c24ce2
John Moss <johnmmoss@gmail.com>
2025-10-10 13:06:35 +0100

    Add secure page and also oidc docker image

---
+++ OAuthSample/OAuthSample.Web/Pages/Secure.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAuthSample.Web.Pages;
+
+public class SecureModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public SecureModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        _logger.LogInformation("The secure page was accessed!");
+    }
+}
